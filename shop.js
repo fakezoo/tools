6efaf158f03bc66a5bd76c4f58f050de2f31a19b
fakezoo <66963403+fakezoo@users.noreply.github.com>
2020-12-28 23:23:50 +0100

    it actually works

--- shop.js
+++ shop.js
@@ -1,10 +1,12 @@
 ///SHOP * SHOP * SHOP///
-var balance = 420
+let balance = 100;
 var itemList = [];
 var shopOutput = document.getElementById("shopOutput");
 var resetButton = document.getElementById("reset");
 var balanceOutput = document.getElementById("balance");
 var shopButtons = document.querySelectorAll(".shopButton");
+
+resetButton.addEventListener("click", reset)
 
 
 // Alle Shopbuttons aus HTML Holen
@@ -16,56 +18,43 @@ for (var i = 0, l = shopButtons.length; i < l; i++) {
     console.log(item)
 }
 
+
 function enterShop() {
     console.log("Shop entered")
 
-    //Jeder Button bekommt die Folgende Funktion
+    //Jeder Item-Button bekommt die Folgende Funktion
     for (var j = 0; j < itemList.length; j++) {
         shopButtons[j].addEventListener("click", shopping)
-            
-    // while (balance < price) {
-    // balanceOutput.classList.add("alert-danger");}
-
-
-
-
-
-
-    //Reset
-    function reset() {
-        //reset Balance and display it, reset Balance warning
-        balance = 420;
-        balanceOutput.innerHTML = balance;
-        balanceOutput.classList.remove("alert-danger")
-
-        //re-enable all buttons
-        for (var j = 0; j < itemList.length; j++) {
-            shopButtons[j].disabled = false;
-
-            // remove items from tray
-            while (shopOutput.hasChildNodes()) {
-                shopOutput.removeChild(shopOutput.firstChild);
-            }
-        }
     }
-}
 
-function shopping(){
-// Nach Click wird bei jedem Button..
-            // der Buttontext gesucht zu einer Variable (selectedItem) gemacht
-            var selectedItem = this.firstChild.nodeValue
 
-            // der Preis der Variable anhand der Position im Array festgelegt
-            // Der Platz im Array von dem Item = der Preis, gerundet und mit random Zahlen verrechnet, damit es realistisch aussieht
-            let price = Math.round(((itemList.indexOf(selectedItem) * 5.9) + 1.2) * 100) / 100;
-            console.log(`${selectedItem} costs ${price} $`);
+    function shopping() {
+        // Bei jedem Shop Button wird auf Click...
+        // der Buttontext gesucht zu einer Variable (selectedItem) gemacht
+        var selectedItem = this.firstChild.nodeValue
 
-            //der Button der geklickt wurde disabled
-            this.disabled = true;
+        // der Preis der Variable anhand der Position im Array festgelegt
+        // Der Platz im Array von dem Item = der Preis, gerundet und mit random Zahlen verrechnet, damit es realistisch aussieht
+        let price = Math.round(((itemList.indexOf(selectedItem) * 5.9) + 1.2) * 100) / 100;
+        console.log(`${selectedItem} costs ${price} $`);
 
-            // die Balance und Balance-Anzeige geupdated
-            balance = balance - price;
-            balanceOutput.innerHTML = Math.round(balance * 100) / 100
+        //der Button der geklickt wurde disabled
+        this.disabled = true;
+
+        // die Balance und Balance-Anzeige geupdated
+        balance = balance - price;
+        balanceOutput.innerHTML = Math.round(balance * 100) / 100
+        console.log("Der Kontostand ist nun " + balance)
+
+        if (balance < 1) {
+            balanceOutput.classList.add("alert-danger");
+            console.log("You Broke, bitch!!")
+            //alle Buttons disabled
+            shopButtons.disabled = true;
+
+        }
+        else {
+
 
             // ein span element namens itemTray created
             var itemTray = document.createElement("span");
@@ -75,5 +64,29 @@ function shopping(){
 
             //das tray mitsamt inhalt an das span shopOutput angehängt
             shopOutput.prepend(itemTray);
+
         }
+    }
+}
+
+
+
+
+//Reset
+function reset() {
+    console.log("You issued a refund!")
+    // Balance wird wieder aufgefüllt 
+    balance = 420;
+    balanceOutput.innerHTML = balance;
+    balanceOutput.classList.remove("alert-danger")
+
+    //re-enable all buttons
+    for (var j = 0; j < itemList.length; j++) {
+        shopButtons[j].disabled = false;
+
+        // remove items from tray
+        while (shopOutput.hasChildNodes()) {
+            shopOutput.removeChild(shopOutput.firstChild);
+        }
+    }
 }
